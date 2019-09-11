c146e057f8970cca5bb06a13498154f09d382364
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-09-10 21:43:06 -0400

    Adding product cards for inventory

--- coleman-defense-solutions/src/App.js
+++ coleman-defense-solutions/src/App.js
@@ -2,34 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/nav"
+import Nav from "./components/nav";
 
 function App() {
   return (
-    <div className="App">
-      <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
-    />
+    <Router>
+      <div className="App">
+        <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
     
-      <Nav/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello! Lets make Coleman Defense Solutions!  
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <Nav/>
+       {/* <Switch>
+        <Route exact path="/" component={() =>
+                <Home
+                  user={this.state.name}
+                  isloggedIn={this.state.isloggedIn}
+                />} />
+
+              <Route exact path="/compact" component={() =>
+                <Home2
+                  user={this.state.name}
+                  isloggedIn={this.state.isloggedIn}
+                />} />
+              <Route exact path="/profile" component={() =>
+                <Profile
+                  user={this.state.name}
+                  isloggedIn={this.state.isloggedIn}
+                />} />
+              </Switch> */}
+            </div>
+   </Router>
+
+  
   );
 }
 
