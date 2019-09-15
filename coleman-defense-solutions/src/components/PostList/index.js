843b57787e6c37925c3154eb7c7d4b548dba688d
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-09-15 19:00:50 -0400

    socket is not constructor

--- coleman-defense-solutions/src/components/PostList/index.js
+++ coleman-defense-solutions/src/components/PostList/index.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react'
 import axios from 'axios'
-
+var Client = require('ftp');
+var csv = require('fast-csv');
+ 
 
 class PostList extends PureComponent {
     constructor(props) {
@@ -14,14 +16,40 @@ class PostList extends PureComponent {
 
 
     componentDidMount() {
-         axios.get('https://jsonplaceholder.typicode.com/posts')
-        //  axios.get('http://webservices.theshootingwarehouse.com/smart/Inventory.asmx')
-         .then(response => {
-             console.log(response)
-             this.setState({posts: response.data
-            })
+        var hostName = "ftp.davidsonsinventory.com";
+        var userName = "ftp58074930-1e";
+        var password = "DavDealerInv";
+        var fileName = 'davidsons_firearm_attributes.csv';
+        var c = new Client();
+        c.on('ready', function() {
+            c.get(fileName, function(err, stream) {
+                if (err) throw err;
+                stream.once('close', function() { c.end(); });
+                csv.fromStream(stream,  {headers: true})
+                    .on("data", function(data){
+                    console.log(data);
+                })
+                    .on("end", function(){
+                        console.log("done");
+                    });
+            });
+        });
+        c.connect({host:hostName,
+            user: userName,
+            password:password
+        })
 
-         })
+        
+
+        
+        //  axios.get('https://jsonplaceholder.typicode.com/posts')
+        // //  axios.get('http://webservices.theshootingwarehouse.com/smart/Inventory.asmx')
+        //  .then(response => {
+        //      console.log(response)
+        //      this.setState({posts: response.data
+        //     })
+
+        //  })
          .catch(error => {
              console.log(error)
              this.setState({errorMsg: 'Error retreiving data'})
@@ -33,12 +61,6 @@ class PostList extends PureComponent {
         return (
             <div> 
                 List of Posts
-                {
-                    posts.length ? 
-                    posts.map(post => <div key={post.id}>{post.title}</div>) :
-                    null
-                }
-                { errorMsg ? <div>{errorMsg}</div> : null}
             </div>
         )
     }
