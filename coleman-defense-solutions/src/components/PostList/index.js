5288ce98c8748581ef31aa0c62b19ba01af6cef8
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-09-15 18:20:25 -0400

    currently I am spitting out a pseudo api and mapping it out, but now need to begin working the .asmx file

--- coleman-defense-solutions/src/components/PostList/index.js
+++ coleman-defense-solutions/src/components/PostList/index.js
@@ -7,38 +7,42 @@ class PostList extends PureComponent {
         super(props)
 
         this.state = {
-            posts: []
+            posts: [],
+            errorMsg: ''
         }
     }
 
 
     componentDidMount() {
-        //  axios.get('https://jsonplaceholder.typicode.com/posts')
-         axios.get('http://webservices.theshootingwarehouse.com/smart/Inventory.asmx')
+         axios.get('https://jsonplaceholder.typicode.com/posts')
+        //  axios.get('http://webservices.theshootingwarehouse.com/smart/Inventory.asmx')
          .then(response => {
              console.log(response)
-             this.setState({posts: response.data})
-             var parseString = require('xml2js').parseString;
-            var xml = "<root>Hello xml2js!</root>"
-            parseString(xml, function (err, result) {
-             console.dir(result, "this is the result");
-});
+             this.setState({posts: response.data
+            })
+//              var parseString = require('xml2js').parseString;
+//             var xml = "<root>Hello xml2js!</root>"
+//             parseString(xml, function (err, result) {
+//              console.dir(result, "this is the result");
+// });
          })
          .catch(error => {
              console.log(error)
+             this.setState({errorMsg: 'Error retreiving data'})
          })
     }
 
     render() {
-        const { posts } = this.state
+        const { posts, errorMsg } = this.state
         return (
             <div> 
                 List of Posts
-                {/* {
+                {
                     posts.length ? 
                     posts.map(post => <div key={post.id}>{post.title}</div>) :
                     null
-                } */}
+                }
+                { errorMsg ? <div>{errorMsg}</div> : null}
             </div>
         )
     }
