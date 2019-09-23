8252a643a925b9a4f29ed76e18ec0d6ec7cc2946
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-09-23 19:20:16 -0400

    parser is parsing in json finally!

--- coleman-defense-solutions/src/components/PostList/index.js
+++ coleman-defense-solutions/src/components/PostList/index.js
@@ -1,8 +1,11 @@
 import React, { PureComponent } from 'react'
 import axios from 'axios'
-var Client = require('ftp');
-var csv = require('fast-csv');
-var parser = require('fast-xml-parser');
+// var Client = require('ftp');
+// var csv = require('fast-csv');
+// var parser = require('fast-xml-parser');
+// var convert = require('xml-js');
+var fs = require('fs'),
+xml2js = require('xml2js');
  
 
 class PostList extends PureComponent {
@@ -17,17 +20,37 @@ class PostList extends PureComponent {
 
 
     componentDidMount() {
+        var jsonStr;
         var jsonObj;
         var xmlData;
-        //  axios.get('http://webservices.theshootingwarehouse.com/smart/inventory.asmx/CategorySearchFieldsDS?CustomerNumber=99994&UserName=99994&Password=99998&Source=47538')
-        axios.get('http://webservices.theshootingwarehouse.com/smart/inventory.asmx/DailyItemUpdate?CustomerNumber=?&UserName=?&Password=?&LastUpdate=1/1/1990&LastItem=-1&Source=?')
+        //  axios.get('http://webservices.theshootingwarehouse.com/smart/inventory.asmx/CategorySearchFieldsDS?CustomerNumber=99994&UserName=99994&Password=99998&Source=')
+        axios.get('http://webservices.theshootingwarehouse.com/smart/inventory.asmx/DailyItemUpdate?CustomerNumber=99994&UserName=99994&Password=99998&LastUpdate=9/17/2019&LastItem=1&Source=')
          .then(xmlData => {
-             console.log(xmlData)
-             this.setState({posts: xmlData.data })
-             jsonObj = parser.parse(xmlData.data);
-              console.log("json", jsonObj)
-        })
-}
+             console.log("XML", xmlData)
+             this.setState({posts: xmlData.data });
+            xmlData = xmlData.data
+             var parser = new xml2js.Parser(/* options */);
+             parser.parseStringPromise(xmlData).then(function (result) {
+               console.log("this is the result", result.string._);
+               console.log('Done');
+               var slightParse = result.string._ 
+               var parser = new xml2js.Parser(/* options */);
+             parser.parseStringPromise(slightParse).then(function (result) {
+               console.log("this is the 2nd result", result);
+               console.log('Done');
+             })
+            })
+             
+           
+             //  jsonStr = parser.parse(xmlData.data);
+            //   console.log("json", jsonStr)
+             
+             //  var result1 = convert.xml2json(xmlData.data, {compact: false, spaces: 6});
+             //  console.log("result1", result1);
+
+              
+         }
+         )}
 
 
 
